595e6c5845c225e9622cb08d0c590d7c9ac39842
Abeer6171 <champchess984@gmail.com>
2025-01-26 22:40:42 +0530

    Add files via upload

---
+++ eligibility check.py
@@ -0,0 +1,15 @@
+print("Hack club project")
+print("I have used python to make this project")
+print("This project check wheather you are eligible or not")
+a=int(input("Enter the age:"))
+if(a>18):
+    print("You are eligible to vote")  
+    print("You are eligible to drive")
+elif(a>=18):
+     print("You are eligible to vote") 
+     print("You are eligible to drive")
+else:
+      print("You are not eligible to Vote")
+      print("You are not eligible to Drive")  
+print("Thanks a lot for seeing my project")
+print("Make sure to give the feedback")
